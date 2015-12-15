a973c28cb4fdf7c9bc466684c703d9f1d273ce32
fernando-fte <fernando.fte@gmail.com>
2015-12-15 10:49:53 -0200

    Finaliza mapeamento do framework "phpSelectSQL"

--- laboratorio/index.php
+++ laboratorio/index.php
@@ -9,38 +9,20 @@
 # inclui funções de tratamento e manipulação para conexão do banco de dados
 include 'phpSelectSQL.php';
 
+/*
+MAPA SELECT
+{"._.list":["table","where","return","regra"],"table":{"._.//":"Dados para tabela a ser selecionada","._.required":true,"._.type":["string"]},"where":{"._.//":"Campos vs valores dos campos da tabela","._.required":true,"._.type":["array"]},"return":{"._.//":"Campos a ser retornado, caso todos use um asterisco \"*\"","._.required":true,"._.type":["string","array"]},"regra":{"._.//":"Conjunto de regras para solicitação do sql","._.required":false,"._.type":["array"],"._.list":["order","limit"],"order":{"._.//":"Ordena as respostas recebidas a partir de array(\"to\", \"by\")","._.required":false,"._.type":["array"],"._.list":["to","by"],"to":{"._.//":"Define o campo a ser ordenado","._.required":false,"._.type":["string"]},"by":{"._.//":"Define a ordem ASC (crescente) ou DESC (decrecente)","._.required":false,"._.type":["string"]}},"limit":{"._.//":"Define o limite de respostas por solicitação, por default não ha limites de resposta","._.required":false,"._.type":["integer"]}}}
+
+MAPA INSERT
+{"._.list":["table","values"],"table":{"._.//":"Dados para tabela a ser selecionada","._.required":true,"._.type":["string"]},"values":{"._.//":"Campos vs valores, relativos aos dados a serem inseridos","._.required":true,"._.type":["array"]}}
+
+MAPA UPDATE
+{"._.list":["table","where","values","return","regra"],"table":{"._.//":"Dados para tabela a ser selecionada","._.required":true,"._.type":["string"]},"where":{"._.//":"Campos vs valores dos campos da tabela","._.required":true,"._.type":["array"]},"values":{"._.//":"Campos vs valores, relativos aos dados a serem inseridos","._.required":true,"._.type":["array"]},"regra":{"._.//":"Conjunto de regras para solicitação do sql","._.required":false,"._.type":["array"],"._.list":["order","limit"],"order":{"._.//":"Ordena as respostas recebidas a partir de array(\"to\", \"by\")","._.required":false,"._.type":["array"],"._.list":["to","by"],"to":{"._.//":"Define o campo a ser ordenado","._.required":false,"._.type":["string"]},"by":{"._.//":"Define a ordem ASC (crescente) ou DESC (decrecente)","._.required":false,"._.type":["string"]}},"limit":{"._.//":"Define o limite de respostas por solicitação, por default não ha limites de resposta","._.required":false,"._.type":["integer"]}}}
+*/
+
 #
 # Fim de "Inclui funções e regras pre-determinadas"
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-# # # # # # # # # # # # # # #  SELECT # # # # # # # # # # # # # # # 
-
-
-
-// $post['table'] = 'base';
-// $post['values']['0'] = 'a123456789';
-// $post['values']['1'] = '{"titulo":"A linda estrada","autor":"Alfredo Gustavo"}';
-// $post['values']['2'] = 'livro';
-// $post['values']['3'] = 'livro';
-// $post['values']['4'] = 'Nome do livro';
-// $post['values']['5'] = '0';
-
-// $done = insert($post, false);
-
-// print_r($done);
-
-$post['table'] = 'base';
-
-$post['where']['2'] = 'livro';
-$post['where']['4'] = 'Nome do livro';
-
-$post['return'] = '1';
-
-$done = select($post, false);
-
-print_r($done);
 
 
 
