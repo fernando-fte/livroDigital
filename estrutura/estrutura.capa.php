ae4a2cede99baa2457e21c2219354318284a212f
fernando-fte <fernando.fte@gmail.com>
2015-12-22 10:06:49 -0200

    Adiciona estrutura separada como php

---
+++ estrutura/estrutura.capa.php
@@ -0,0 +1,68 @@
+<!-- Contents para capa -->
+<div id="capa" class="page page-cover">
+
+	<!-- Conteudo da capa (titulo e autores) -->
+	<div class="capa-contents">
+		<span class="capa-titulo"></span>
+		<span class="capa-autor"></span>
+	</div>
+
+	<!-- MOBILE: Content logo para  -->
+	<div class="capa-logo-top">
+		<img src="#{url}/logo.white.svg">
+	</div>
+
+	<!-- Botão do menu -->
+	<div class="capa-menu bnt-menu">
+		<span class="fa fa-bars"></span>
+	</div>
+
+	<!-- Botão do grupo de botões da navegação -->
+	<div class="capa-btn-group">
+
+		<!-- Botão para sumário -->
+		<div class="btn-nav-item">
+			<span class="fa fa-list"></span>
+			<span class="label">Sumário</span>
+		</div>
+
+		<!-- Botão para atividades -->
+		<div class="btn-nav-item">
+			<span class="fa fa-pencil"></span>
+			<span class="label">Atividades</span>
+		</div>
+
+		<!-- Botão para continuar leitura -->
+		<div class="btn-nav-item">
+			<span class="fa fa-map-marker"></span>
+			<span class="label">Continuar de onde parei</span>
+		</div>
+
+		<!-- Botão para anotações -->
+		<div class="btn-nav-item">
+			<span class="fa fa-bookmark-o"></span>
+			<span class="label">Anotações</span>
+		</div>
+
+		<!-- Botão para refrerências -->
+		<div class="btn-nav-item">
+			<span class="fa fa-connectdevelop"></span>
+			<span class="label">Referências</span>
+		</div>
+	</div>
+
+	<!-- Botão iniciar leitura -->
+	<div class="capa-btn-iniciar">
+		<span class="label">Inicar leitura</span>
+
+		<div class="btn-iniciar">
+			<span class="fa fa-angle-right"></span>
+		</div>
+	</div>
+
+	<!-- Contents para "pos" e logo -->
+	<div class="capa-logo">
+		<img src="#{url}/logo.white.svg">
+		<img src="#{url}/pos.white.svg">
+	</div>
+</div>
