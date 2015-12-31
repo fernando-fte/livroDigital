ad815ed7ca526aa846a9403c7873fba3e3e0f0a3
fernando-fte <fernando.fte@gmail.com>
2015-12-31 12:41:08 -0200

    Configura tipografia e alinhamento col-grid

--- estrutura/sumario.php
+++ estrutura/sumario.php
@@ -3,51 +3,57 @@
 <!-- Reservatório de conteúdos -->
 <div class="contents">
 
-		<!-- Sumario -->
-		<section id="sumario" class="app-sum-contents section-page">
+	<!-- Sumario -->
+	<section id="sumario" class="app-sum-contents section-page">
 
-			<!-- Gruop para cada capitulo -->
-			<ul class="app-sum-section">
+		<!-- Gruop para cada capitulo -->
+		<div class="app-sum-section | col-md-2 col-sm-2 col-ss-2 col-xs-2">
 
-				<!-- Titulo do capitulo -->
-				<li class="app-sum-section-item app-sum-section-header">
+			<!-- Titulo do capitulo -->
+			<div class="app-sum-section-item app-sum-section-header | app-font-app | col-md-12 col-sm-12 col-ss-12 col-xs-12">
 
-					<!-- Icone do botão indicador -->
-					<span class="app-sum-btn-icon"></span>
+				<!-- Icone do botão indicador -->
+				<span class="app-sum-btn-icon | col-md-1 col-sm-1 col-ss-1 col-xs-1 | font-md-2 font-sm-2 font-ss-2 font-xs-2"></span>
 
+				<!-- COntents do header -->
+				<div class="app-sum-section-header-contents | col-md-11 col-sm-11 col-ss-11 col-xs-11">
+					
 					<!-- Nome da seção (unidade I|II|III / abertura ...) -->
-					<span class="app-sum-section-secao">Capítulo I</span>
+					<span class="app-sum-section-secao | app-cor-black-80 | text-uppercase text-bold | col-md-12 col-sm-12 col-ss-12 col-xs-12 | font-md-3 font-sm-3 font-ss-3 font-xs-3">Capítulo I</span>
 
 					<!-- Nome caso seja titulo -->
-					<span class="app-sum-section-titulo">A função financeira, análise das demonstrações financeiras</span>
-				</li>
+					<span class="app-sum-section-titulo | app-cor-black-60 | col-md-12 col-sm-12 col-ss-12 col-xs-12 | font-md-small-4 font-sm-small-4 font-ss-small-4 font-xs-small-4">A função financeira, análise das demonstrações financeiras</span>
+				</div>
+			</div>
 
-				<!-- Itens do sumario -->
-				<li class="app-sum-section-item">
+			<!-- Itens do sumario -->
+			<div class="app-sum-section-item | app-font-header | col-md-12 col-sm-12 col-ss-12 col-xs-12 | font-md-2 font-sm-2 font-ss-2 font-xs-2">
 
-					<!-- Icone do botão indicador -->
-					<span class="app-sum-btn-icon"></span>
+				<!-- Icone do botão indicador -->
+				<span class="app-sum-btn-icon | col-md-1 col-sm-1 col-ss-1 col-xs-1"></span>
 
-					<!-- Nome da seção -->
-					<span class="sum-section-name">Objetivos da administração financeira e seus conflitos</span>
+				<!-- Nome da seção -->
+				<span class="sum-section-name | app-cor-black-80 | text-uppercase text-bold | col-md-10 col-sm-10 col-ss-10 col-xs-10">Objetivos da administração financeira e seus conflitos</span>
 
-					<!-- Icone do botão -->
-					<span class="app-sum-btn-icon-sub"></span>
+				<!-- Icone do botão -->
+				<span class="app-sum-btn-icon-sub | col-md-1 col-sm-1 col-ss-1 col-xs-1 | font-md-3 font-sm-3 font-ss-3 font-xs-3"></span>
 
-					<!-- Gruop para cada capitulo -->
-					<ul class="app-sum-section-sub">
+				<!-- Gruop para cada capitulo -->
+				<div class="app-sum-section-sub | col-md-12 col-sm-12 col-ss-12 col-xs-12">
 
-						<!-- Sub-itens do sumario -->
-						<li class="app-sum-section-sub-item">
+					<span class="col-md-1 col-sm-1 col-ss-1 col-xs-1"></span>
 
-							<!-- Icone do botão indicador -->
-							<span class="app-sum-btn-icon">Objetivos da administração financeira e seus conflitos</span>
+					<!-- Sub-itens do sumario -->
+					<div class="app-sum-section-sub-item | app-font-header | col-md-11 col-sm-11 col-ss-11 col-xs-11">
 
-							<!-- Nome da seção -->
-							<span class="app-sum-section-sub-name"></span>
-						</li>
-					</ul>
-				</li>
-			</ul>
-		</section>
+						<!-- Icone do botão indicador -->
+						<span class="app-sum-btn-icon | app-cor-black-80 | col-md-1 col-sm-1 col-ss-1 col-xs-1 | font-md-2 font-sm-2 font-ss-2 font-xs-2">Objetivos da administração financeira e seus conflitos</span>
+
+						<!-- Nome da seção -->
+						<span class="app-sum-section-sub-name | col-md-11 col-sm-11 col-ss-11 col-xs-11 | font-md-small-2 font-sm-small-2 font-ss-small-2 font-xs-small-2"></span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 </div>
