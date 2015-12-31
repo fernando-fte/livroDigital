f28e9f1a741c4169fad992e818fcad5596de0c7c
fernando-fte <fernando.fte@gmail.com>
2015-12-31 11:47:59 -0200

    Estrutura html do sumário configurada

---
+++ estrutura/sumario.php
@@ -0,0 +1,53 @@
+<?php include $settings['pages']['menu'] ?>
+
+<!-- Reservatório de conteúdos -->
+<div class="contents">
+
+		<!-- Sumario -->
+		<section id="sumario" class="app-sum-contents section-page">
+
+			<!-- Gruop para cada capitulo -->
+			<ul class="app-sum-section">
+
+				<!-- Titulo do capitulo -->
+				<li class="app-sum-section-item app-sum-section-header">
+
+					<!-- Icone do botão indicador -->
+					<span class="app-sum-btn-icon"></span>
+
+					<!-- Nome da seção (unidade I|II|III / abertura ...) -->
+					<span class="app-sum-section-secao">Capítulo I</span>
+
+					<!-- Nome caso seja titulo -->
+					<span class="app-sum-section-titulo">A função financeira, análise das demonstrações financeiras</span>
+				</li>
+
+				<!-- Itens do sumario -->
+				<li class="app-sum-section-item">
+
+					<!-- Icone do botão indicador -->
+					<span class="app-sum-btn-icon"></span>
+
+					<!-- Nome da seção -->
+					<span class="sum-section-name">Objetivos da administração financeira e seus conflitos</span>
+
+					<!-- Icone do botão -->
+					<span class="app-sum-btn-icon-sub"></span>
+
+					<!-- Gruop para cada capitulo -->
+					<ul class="app-sum-section-sub">
+
+						<!-- Sub-itens do sumario -->
+						<li class="app-sum-section-sub-item">
+
+							<!-- Icone do botão indicador -->
+							<span class="app-sum-btn-icon">Objetivos da administração financeira e seus conflitos</span>
+
+							<!-- Nome da seção -->
+							<span class="app-sum-section-sub-name"></span>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</section>
+</div>
