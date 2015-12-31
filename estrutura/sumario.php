e0b0e92c91fdbd674c88cafaef428569107773c9
fernando-fte <fernando.fte@gmail.com>
2015-12-31 14:04:16 -0200

    Configura botões e altura dos itens

--- estrutura/sumario.php
+++ estrutura/sumario.php
@@ -10,47 +10,45 @@
 		<div class="app-sum-section | col-md-2 col-sm-2 col-ss-2 col-xs-2">
 
 			<!-- Titulo do capitulo -->
-			<div class="app-sum-section-item app-sum-section-header | app-font-app | col-md-12 col-sm-12 col-ss-12 col-xs-12">
+			<div class="app-sum-section-item app-sum-section-header | app-font-app | padding-none-width | col-md-12 col-sm-12 col-ss-12 col-xs-12">
 
 				<!-- Icone do botão indicador -->
-				<span class="app-sum-btn-icon | app-bg-black-60 | col-md-1 col-sm-1 col-ss-1 col-xs-1 | font-md-2 font-sm-2 font-ss-2 font-xs-2"></span>
+				<span class="app-sum-btn-icon | app-bg-black-60 app-cor-white | col-md-1 col-sm-1 col-ss-1 col-xs-1 | font-md-2 font-sm-2 font-ss-2 font-xs-2"></span>
 
 				<!-- COntents do header -->
 				<div class="app-sum-section-header-contents | col-md-11 col-sm-11 col-ss-11 col-xs-11">
 					
 					<!-- Nome da seção (unidade I|II|III / abertura ...) -->
-					<span class="app-sum-section-secao | app-cor-black-80 | text-uppercase text-bold | col-md-12 col-sm-12 col-ss-12 col-xs-12 | font-md-3 font-sm-3 font-ss-3 font-xs-3">Capítulo I</span>
+					<span class="app-sum-section-secao | app-cor-black-80 | text-uppercase text-bold | padding-none-width | col-md-12 col-sm-12 col-ss-12 col-xs-12 | font-md-3 font-sm-3 font-ss-3 font-xs-3">Capítulo I</span>
 
 					<!-- Nome caso seja titulo -->
-					<span class="app-sum-section-titulo | app-cor-black-60 | col-md-12 col-sm-12 col-ss-12 col-xs-12 | font-md-small-4 font-sm-small-4 font-ss-small-4 font-xs-small-4">A função financeira, análise das demonstrações financeiras</span>
+					<span class="app-sum-section-titulo | app-cor-black-60 | padding-none-width | col-md-12 col-sm-12 col-ss-12 col-xs-12 | font-md-small-4 font-sm-small-4 font-ss-small-4 font-xs-small-4">A função financeira, análise das demonstrações financeiras</span>
 				</div>
 			</div>
 
 			<!-- Itens do sumario -->
-			<div class="app-sum-section-item | app-font-header | col-md-12 col-sm-12 col-ss-12 col-xs-12 | font-md-2 font-sm-2 font-ss-2 font-xs-2">
+			<div class="app-sum-section-item | app-font-header | padding-none-width | col-md-12 col-sm-12 col-ss-12 col-xs-12 | font-md-2 font-sm-2 font-ss-2 font-xs-2">
 
 				<!-- Icone do botão indicador -->
-				<span class="app-sum-btn-icon | col-md-1 col-sm-1 col-ss-1 col-xs-1"></span>
+				<span class="app-sum-btn-icon |  app-bg-pattern-40 app-cor-white | col-md-1 col-sm-1 col-ss-1 col-xs-1"></span>
 
 				<!-- Nome da seção -->
 				<span class="sum-section-name | app-cor-black-80 | text-uppercase text-bold | col-md-10 col-sm-10 col-ss-10 col-xs-10">Objetivos da administração financeira e seus conflitos</span>
 
 				<!-- Icone do botão -->
-				<span class="app-sum-btn-icon-sub | col-md-1 col-sm-1 col-ss-1 col-xs-1 | font-md-3 font-sm-3 font-ss-3 font-xs-3"></span>
+				<span class="app-sum-btn-icon-sub | app-cor-pattern | col-md-1 col-sm-1 col-ss-1 col-xs-1 | font-md-3 font-sm-3 font-ss-3 font-xs-3"></span>
 
 				<!-- Gruop para cada capitulo -->
 				<div class="app-sum-section-sub | col-md-12 col-sm-12 col-ss-12 col-xs-12">
 
-					<span class="col-md-1 col-sm-1 col-ss-1 col-xs-1"></span>
-
 					<!-- Sub-itens do sumario -->
-					<div class="app-sum-section-sub-item | app-font-header | col-md-11 col-sm-11 col-ss-11 col-xs-11">
+					<div class="app-sum-section-sub-item | app-font-header | padding-none-width | col-md-11 col-sm-11 col-ss-11 col-xs-11 | col-md-offset-1 col-sm-offset-1 col-ss-offset-1 col-xs-offset-1">
 
 						<!-- Icone do botão indicador -->
-						<span class="app-sum-btn-icon | app-cor-black-80 | col-md-1 col-sm-1 col-ss-1 col-xs-1 | font-md-2 font-sm-2 font-ss-2 font-xs-2">Objetivos da administração financeira e seus conflitos</span>
+						<span class="app-sum-btn-icon | app-cor-black-80 | col-md-1 col-sm-1 col-ss-1 col-xs-1 | font-md-small-2 font-sm-small-2 font-ss-small-2 font-xs-small-2"></span>
 
 						<!-- Nome da seção -->
-						<span class="app-sum-section-sub-name | col-md-11 col-sm-11 col-ss-11 col-xs-11 | font-md-small-2 font-sm-small-2 font-ss-small-2 font-xs-small-2"></span>
+						<span class="app-sum-section-sub-name | col-md-11 col-sm-11 col-ss-11 col-xs-11 | font-md-2 font-sm-2 font-ss-2 font-xs-2">Objetivos da administração financeira e seus conflitos</span>
 					</div>
 				</div>
 			</div>
