f3122148cd38bd307b5c89d30b959c1de05769e7
fernando-fte <fernando.fte@gmail.com>
2016-01-06 17:48:07 -0200

    Adiciona script app e iniciador de vendors

---
+++ scripts/vendors.js
@@ -0,0 +1,17 @@
+function changeSize() {
+	var width = parseInt($("#Width").val());
+	var height = parseInt($("#Height").val());
+	$(".add-scroll").width(width).height(height);
+	// update scrollbars
+	$('.add-scroll').perfectScrollbar('update');
+	// or even with vanilla JS!
+	Ps.update(document.getElementById('add-scroll'));
+}
+$(function() {
+	$('.add-scroll').perfectScrollbar();
+	// with vanilla JS!
+	Ps.initialize(document.getElementById('add-scroll'));
+});
+
+// Adiciona slider na barra do menu
+$('#app-nav-light-density').slider({ formatter: function(value) { return 'Densidade da luz: ' + value; } });
