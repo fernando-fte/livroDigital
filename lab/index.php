47072d7284a07ff66644e17c26e19fb554c21b7c
fernando-fte <fernando.fte@gmail.com>
2016-01-11 08:36:06 -0200

    Teste de envio de html para php

--- lab/index.php
+++ lab/index.php
@@ -1,3 +1,11 @@
+<?php 
+	if (array_key_exists('ajax', $_POST)) {
+
+		print_r($_POST['ajax']);
+	}
+	else {
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -6,17 +14,23 @@
 	<body>
 
 
-<ul>
-  <li>list item 1</li>
-  <img src="">
-  <li>list item 2</li>
-  <li class="third-item">list item 3</li>
-  <li>list item 4</li>
-  <li>list item 5</li>
-</ul>
+	<ul id="lista">
+	  <li>list item 1</li>
+	  <img src="#">
+	  <li>list item 2</li>
+	  <li class="third-item">list item 3</li>
+	  <li>list item 4</li>
+	  <li>list item 5</li>
+	</ul>
 
+	<button id="enviar">Enviar</button>
 
 	</body>
 	<script type="text/javascript" src="scripts/jquery.min.js"></script>
-	<script type="text/javascript" src="scripts/app.js"></script>
+	<script type="text/javascript" src="scripts/coffee-script.js"></script>
+	<script type="text/coffeescript" src="scripts/app.coffee"></script>
 </html>
+
+<?php 
+	} // FIm do callbak
+?>
