8140be803cab7efb7acb136df1da0eb5a3096bc4
fernando-fte <fernando.fte@gmail.com>
2016-01-10 23:54:45 -0200

    Inicia testes

--- lab/index.php
+++ lab/index.php
@@ -1,1 +1,22 @@
-﻿<?php?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Título</title>
+	</head>
+	<body>
+
+
+<ul>
+  <li>list item 1</li>
+  <img src="">
+  <li>list item 2</li>
+  <li class="third-item">list item 3</li>
+  <li>list item 4</li>
+  <li>list item 5</li>
+</ul>
+
+
+	</body>
+	<script type="text/javascript" src="scripts/jquery.min.js"></script>
+	<script type="text/javascript" src="scripts/app.js"></script>
+</html>
---
+++ lab/scripts/app.coffee
@@ -0,0 +1,1 @@
+$("img").next($(".third-item"))
